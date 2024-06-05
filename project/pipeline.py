e47932e7e53227df1a38487a7ce879162684c242
Xinia Apchora <xiniaapchora@Xinias-Laptop.local>
2024-06-06 01:39:14 +0200

    project_pipeline

--- project/pipeline.py
+++ project/pipeline.py
@@ -1,61 +1,59 @@
-import requests
-import zipfile
-import io
 import pandas as pd
+import requests
+from io import BytesIO
 import sqlite3
+import os
+import zipfile
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
 
-# URLs for the datasets
-urls = {
-    "air_quality": "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/openaq/exports/csv?lang=en&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B",
-    "crop_production_fao": "https://bulks-faostat.fao.org/production/Production_Crops_Livestock_E_Europe.zip"
-}
+urllib3.disable_warnings(InsecureRequestWarning)
+    
 
-# Function to download and read a CSV file directly from URL
-def read_csv_from_url(url, sep=';', encoding='utf-8'):
-    response = requests.get(url)
-    response.raise_for_status()  # Check for request errors
-    return pd.read_csv(io.StringIO(response.text), sep=sep, encoding=encoding)
+def fetch_csv(url, sep=';', encoding='utf-8'):
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    df = pd.read_csv(BytesIO(response.content), sep=sep, encoding=encoding)
+    return df
 
-# Function to download and extract a specific file from a ZIP archive
-def download_and_extract_csv_from_zip(url):
-    response = requests.get(url)
-    response.raise_for_status()  # Check for request errors
-    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
-        # Identify the CSV file with the expected data
-        for file_name in zip_ref.namelist():
+def save_sqlite(df, table_name, db_path):
+    conn = sqlite3.connect(db_path)
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
+    conn.close()
+
+
+def download_zip(url, headers=None):
+    response = requests.get(url, headers=headers, verify=False)
+    response.raise_for_status()
+    with zipfile.ZipFile(BytesIO(response.content)) as zfile:
+        for file_name in zfile.namelist():
             if file_name.endswith('.csv') and 'Europe' in file_name:
-                with zip_ref.open(file_name) as extracted_file:
-                    return pd.read_csv(extracted_file, encoding='latin1')
-    raise ValueError("No suitable CSV file found in the ZIP archive")
+                with zfile.open(file_name) as extracted_file:
+                    df =  pd.read_csv(extracted_file, encoding='latin1')
+    return df
 
-# Read air quality data directly from URL
-air_quality_data = read_csv_from_url(urls["air_quality"])
 
-# Download and read specific file from crop production data (FAO) ZIP
-crop_production_fao_data = download_and_extract_csv_from_zip(urls["crop_production_fao"])
+url_1 = 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/openaq/exports/csv?lang=en&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B'
+url_2 = 'https://bulks-faostat.fao.org/production/Production_Crops_Livestock_E_Europe.zip'
 
-# Filter air quality data for relevant columns and Germany
-air_quality_data = air_quality_data[
-    ['Country Code', 'City', 'Location', 'Pollutant', 'Value', 'Last Updated']
-]
-air_quality_data = air_quality_data[air_quality_data['Country Code'] == 'DE']
+db_path = os.path.join('../data', 'data_base.db')
 
-# Filter crop production data for relevant columns and Germany
-crop_production_fao_data = crop_production_fao_data[
-    ['Area', 'Item', 'Element', 'Unit', 'Y2018', 'Y2019', 'Y2020', 'Y2021', 'Y2022']
-]
-crop_production_fao_data = crop_production_fao_data[crop_production_fao_data['Area'] == 'Germany']
+df1 = fetch_csv(url_1)
+df2 = download_zip(url_2)
 
-# Clean the data (example: handle missing values)
-air_quality_data = air_quality_data.dropna()
-crop_production_fao_data = crop_production_fao_data.dropna()
+df2 = df2[['Area', 'Item', 'Element', 'Y2018', 'Y2019', 'Y2020', 'Y2021', 'Y2022']]
+df2 = df2.fillna(0)
 
-# Store the cleaned data in SQLite database
-database_path = 'data_analysis.db'
-conn = sqlite3.connect(database_path)
-air_quality_data.to_sql('air_quality', conn, if_exists='replace', index=False)
-crop_production_fao_data.to_sql('crop_production', conn, if_exists='replace', index=False)
-conn.close()
+df1 = df1[['City', 'Pollutant', 'Value', 'Last Updated','Country Label']]
 
-# Print success message
-print(f"Data pipeline executed successfully.")
+df1 = df1[df1['Country Label'] == 'Germany']
+
+df1['Last Updated'] = pd.to_datetime(df1['Last Updated'], errors='coerce', utc=True)
+df1 = df1[(df1['Last Updated'] >= pd.Timestamp('2018-01-01', tz='UTC')) & (df1['Last Updated'] <= pd.Timestamp('2022-12-31', tz='UTC'))]
+
+
+df1['Last Updated'] = df1['Last Updated'].dt.year
+df1= df1.fillna('Other')
+
+save_sqlite(df1, 'air_quality', db_path)
+save_sqlite(df2, 'crop_production', db_path)
