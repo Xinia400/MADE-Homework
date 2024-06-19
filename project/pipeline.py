487b061fbcc9c50f8161ac2a31f30a609fee9f4c
Xinia Apchora <xiniaapchora@Xinias-Laptop.local>
2024-06-19 22:26:41 +0200

    pipeline_fix

--- project/pipeline.py
+++ project/pipeline.py
@@ -16,10 +16,7 @@ def fetch_csv(url, sep=';', encoding='utf-8'):
     df = pd.read_csv(BytesIO(response.content), sep=sep, encoding=encoding)
     return df
 
-def save_sqlite(df, table_name, db_path):
-    conn = sqlite3.connect(db_path)
-    df.to_sql(table_name, conn, if_exists='replace', index=False)
-    conn.close()
+
 
 
 def download_zip(url, headers=None):
